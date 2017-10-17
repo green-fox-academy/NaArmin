fcfa44c142bb9f5caed8e5d175bbf791606d82d3
NaÁrmin <nagym.armin@gmail.com>
2017-10-17 11:16:38 +0200

    First 2 progs of day 2

---
+++ week-03/day-2/01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+	int  i = 2;
+	float f = 13.5, result;
+
+	//TODO: print out the f/i quotient
+	result = f / i;
+        printf("%f\n", result);
+	//QUESTION: do you need explicit conversion? -NO.
+
+
+	return 0;
+}
+
