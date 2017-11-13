e6e42bc2f3239a8fcec081eff037ba724735c5c9
NaÁrmin <nagym.armin@gmail.com>
2017-11-13 11:40:38 +0100

    First c++ program!

---
+++ week-06/day-1/00.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string n;
+    cout << "Hi! What's your name? ";
+    cin >> n;
+    cout << "Hello " << n << "!" << endl;
+    return 0;
+}
+
