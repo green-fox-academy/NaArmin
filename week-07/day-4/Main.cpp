c2d9e4689d2f7198e7ab0690dbd936c23f981944
NaÁrmin <nagym.armin@gmail.com>
2017-11-23 13:01:36 +0100

    basic saving to vector, print usage, listing vector, exit

--- week-07/day-4/Main.cpp
+++ week-07/day-4/Main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <vector>
 
 #include "SerialPortWrapper.h"
 
 using namespace std;
+
+void usageprint();
 
 int main()
 {
@@ -16,15 +19,44 @@ int main()
 
     // connection
 
-        SerialPortWrapper *serial = new SerialPortWrapper("COM6", 115200);
-        serial->openPort();
-        string line;
-        while(1){
+    SerialPortWrapper *serial = new SerialPortWrapper("COM19", 115200);
+    serial->openPort();
+    string line;
+
+    vector<string> loggedtemps;
+    char c;
+    bool portopen;
+    bool logging;
+    bool run = true;
+    usageprint();
+    while(run){
         serial->readLineFromPort(&line);
         if (line.length() > 0){
-        cout << line << endl;
+             loggedtemps.push_back(line);
         }
+        c = getchar();
+        switch (c) {
+        case 'e' :
+            run = false;
+            break;
+        case 'l' :
+            for (unsigned int i = 0; i < loggedtemps.size(); ++i)
+                cout << loggedtemps[i] << endl;
         }
+    }
         serial->closePort();
     return 0;
+}
+void usageprint()
+{
+cout << "\
+Temperature Logger Application\n\
+==============================\n\
+Commands:\n\
+ h        Show command list\n\
+ o        Open port\n\
+ s        Start logging / Stop logging\n\
+ c        Close port\n\
+ l        List after error handling\n\
+ e        Exit from the program\n\n";
 }
