59e1a5212542cd7070c1b718c823c13f1213f531
NaÁrmin <nagym.armin@gmail.com>
2017-11-23 14:19:05 +0100

    basic commands work with error messages, no input error handling

--- week-07/day-4/Main.cpp
+++ week-07/day-4/Main.cpp
@@ -20,31 +20,66 @@ int main()
     // connection
 
     SerialPortWrapper *serial = new SerialPortWrapper("COM19", 115200);
-    serial->openPort();
     string line;
 
     vector<string> loggedtemps;
-    char c;
-    bool portopen;
-    bool logging;
+    bool portopen = false;
+    bool logging = false;
     bool run = true;
     usageprint();
     while(run){
-        serial->readLineFromPort(&line);
-        if (line.length() > 0){
+        if (portopen)
+            serial->readLineFromPort(&line);
+        if (logging && (line.length() > 0))
              loggedtemps.push_back(line);
-        }
-        c = getchar();
-        switch (c) {
-        case 'e' :
+        switch (getchar()) {
+        case 'e':
             run = false;
             break;
-        case 'l' :
+        case 'l':
+            cout << "Recorded temperatures:\n--- Date ------ Time ------ Degrees" << endl;
             for (unsigned int i = 0; i < loggedtemps.size(); ++i)
                 cout << loggedtemps[i] << endl;
+            break;
+        case 'o':
+            if (!portopen) {
+                serial->openPort();
+                portopen = true;
+                cout << "Port opened." << endl;
+            } else {
+                cout << "Port is already open!" << endl;
+            }
+            break;
+        case 's':
+            if (portopen)
+                if (!logging) {
+                    logging = true;
+                    cout << "Logging started." << endl;
+                } else {
+                    logging = false;
+                    cout << "Logging stopped." << endl;
+                }
+            else
+                cout << "Port is not open!" << endl;
+            break;
+        case 'c':
+            if (portopen) {
+                serial->closePort();
+                portopen = false;
+                logging = false;
+                cout << "Port closed." << endl;
+            } else {
+                cout << "Port is already closed!" << endl;
+            }
+            break;
+        case 'h':
+            usageprint();
+            break;
         }
     }
+    if (portopen)
         serial->closePort();
+    delete serial;
     return 0;
 }
 void usageprint()
