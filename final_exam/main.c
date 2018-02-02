318a455eadaf7bc61091e62448ce36d933eaf696
NaArmin <nagym.armin@gmail.com>
2018-02-02 10:28:57 +0100

    final exam task 12 2nd version

--- final_exam/main.c
+++ final_exam/main.c
@@ -5,8 +5,8 @@ double arr_sum_avg(int*, int*);
 
 int main()
 {
-    int* first_array = {10, 20, 30, 40, 50, 60};
-    int* second_array = {2, 4, 8, 16};
+    int first_array[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int second_array[] = {2, 4, 8, 16};
 
     arr_sum_avg(first_array, second_array);
     printf("Sum of the 2 arrays' average:\n");
@@ -16,8 +16,16 @@ double arr_sum_avg(int* arr_in1, int* arr_in2)
 {
     double avg = 0;
     long sum = 0;
-    for (int i = 0; i < sizeof(arr_in1) - 1; ++i) {
+    int i;
+    for (i = 0; i < sizeof(arr_in1); i++) {
         sum += arr_in1[i];
+        printf("%d ", arr_in1[i]);
     }
-    printf("%d", i);
+    printf("%d   ", i);
+    for (i = 0; i < sizeof(arr_in2); i++) {
+        sum += arr_in2[i];
+        printf("%d ", arr_in2[i]);
+    }
+    printf("%d   ", i);
+    return sum;
 }
